3ee84ea49731433d4657c95efd01d02fe97747e7
Thinh Nguyen <thinhtrung.nguyen1701@gmail.com>
2023-01-04 14:22:00 -0800

    frame-one

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function menuToggle(){
+    var toggle = document.getElementById("links");
+    if (toggle.style.display === "block"){
+        toggle.style.display = "none";
+    } else {
+        toggle.style.display = "block";
+    }
+}
+
+function listProducts(){
+    var listP = document.getElementById("listProducts");
+    if (listP.style.display === "block"){
+        listP.style.display = "none";
+    } else {
+        listP.style.display = "block";
+    }
+}
